dc86d575d88fb2527072197ab782b1f328144278
Lucas <lucas.g.oliveira1998@outlook.com>
2023-10-05 18:02:12 -0300

    Criando primeiro controller e repositorio

---
+++ src/main/java/com/devcaotics/doapet/repositories/OngRepository.java
@@ -0,0 +1,7 @@
+package com.devcaotics.doapet.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devcaotics.doapet.entities.Ong;
+
+public interface OngRepository extends JpaRepository<Ong, Long> {}
