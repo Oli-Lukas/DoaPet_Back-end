6156066d3fca1f508c1df1c27c8f9338588824d3
Lucas de Oliveira <lucas.g.oliveira1998@outlook.com>
2023-11-15 23:16:37 -0300

    Habilitando o cors no spring security

--- src/main/java/com/example/doapet/security/SegurancaConfiguration.java
+++ src/main/java/com/example/doapet/security/SegurancaConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -33,6 +34,7 @@ public class SegurancaConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpsecurity) throws Exception {
         return httpsecurity
                 .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
