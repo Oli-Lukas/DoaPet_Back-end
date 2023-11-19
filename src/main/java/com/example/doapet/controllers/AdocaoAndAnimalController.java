ced604c0a3a59ed92a5ac2706a581017f8d65913
Lucas de Oliveira <lucas.g.oliveira1998@outlook.com>
2023-11-19 20:05:54 -0300

    Adicionando 'CrossOrigin' annotation nas classes controllers

--- src/main/java/com/example/doapet/controllers/AdocaoAndAnimalController.java
+++ src/main/java/com/example/doapet/controllers/AdocaoAndAnimalController.java
@@ -22,15 +22,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("adocao")
+@CrossOrigin(origins = "*")
 public class AdocaoAndAnimalController {
 
     @Autowired
