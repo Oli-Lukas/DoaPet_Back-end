f266b42f6137e350e70bb56c3b246283b7bf1030
Lucas de Oliveira <lucas.g.oliveira1998@outlook.com>
2023-12-19 21:27:34 -0300

    Refatorando arquivo 'SecurityFilter.java' visando legibilidade

--- src/main/java/com/example/doapet/security/SecurityFilter.java
+++ src/main/java/com/example/doapet/security/SecurityFilter.java
@@ -19,32 +19,40 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
-public class SecurityFilter extends OncePerRequestFilter{
+public class SecurityFilter extends OncePerRequestFilter {
 
     @Autowired
     TokenService tokenService;
+
     @Autowired
-    UsuarioRepository userRepository;
+    UsuarioRepository usuarioRepository;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        FilterChain filterChain
+    ) throws ServletException, IOException {
+
         var token = this.recoverToken(request);
-        if(token != null){
+
+        if(token != null) {
+
             var login = tokenService.validateToken(token);
-            
-            UserDetails user = userRepository.findByEmail(login).get();
+            UserDetails user = usuarioRepository.findByEmail(login).get();
 
             var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
+
         filterChain.doFilter(request, response);
     }
 
-    private String recoverToken(HttpServletRequest request){
+    private String recoverToken(HttpServletRequest request) {
+
         var authHeader = request.getHeader("Authorization");
-        System.out.println(authHeader);
         if(authHeader == null) return null;
+
         return authHeader.replace("Bearer ", "");
     }
-    
 }
