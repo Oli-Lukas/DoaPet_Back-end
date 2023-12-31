d76d910a02e631151cdfeb60f3c15cd756d532b5
Lucas de Oliveira <lucas.g.oliveira1998@outlook.com>
2023-12-31 11:08:26 -0300

    Refatorando arquivo 'Usuario.java'
    Removendo import e comentarios desnecessarios.

--- src/main/java/com/example/doapet/model/Usuario.java
+++ src/main/java/com/example/doapet/model/Usuario.java
@@ -1,14 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.example.doapet.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
