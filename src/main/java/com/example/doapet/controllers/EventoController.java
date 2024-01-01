5819aa7f73b3eac5a28c21fc1a36164b6dd5cbf3
Lucas de Oliveira <lucas.g.oliveira1998@outlook.com>
2024-01-01 19:15:34 -0300

    Adicionando rota para cadastrar evento

--- src/main/java/com/example/doapet/controllers/EventoController.java
+++ src/main/java/com/example/doapet/controllers/EventoController.java
@@ -1,10 +1,23 @@
 package com.example.doapet.controllers;
 
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.doapet.dto.request.CadastrarEventoRequest;
+import com.example.doapet.model.Evento;
+import com.example.doapet.model.TipoUsuario;
+import com.example.doapet.model.Usuario;
 import com.example.doapet.repository.EventoRepository;
+import com.example.doapet.repository.UsuarioRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,4 +28,31 @@ import lombok.RequiredArgsConstructor;
 public class EventoController {
   
   private final EventoRepository eventoRepository;
+  private final UsuarioRepository usuarioRepository;
+
+  @PostMapping("/")
+  public ResponseEntity<Void> cadastrarEvento(@RequestBody CadastrarEventoRequest request) {
+
+    UserDetails userDetails =(UserDetails) SecurityContextHolder
+                                            .getContext()
+                                            .getAuthentication()
+                                            .getPrincipal();
+
+    Optional<Usuario> optionalCurrentUser = this.usuarioRepository.findByEmail(userDetails.getUsername());
+    if (optionalCurrentUser.isEmpty()) return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+    Usuario currentUser = optionalCurrentUser.get();
+    if (currentUser.getTipoUsuario() == TipoUsuario.INDIVIDUAL) return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+
+    Evento evento = new Evento(
+      request.getNomeDoEvento(),
+      request.getDescricao(),
+      request.getDataDoEvento(),
+      currentUser.getEndereco(),
+      currentUser
+    );
+    this.eventoRepository.save(evento);
+
+    return ResponseEntity.status(HttpStatus.CREATED).build();
+  }
 }
--- src/main/java/com/example/doapet/model/Evento.java
+++ src/main/java/com/example/doapet/model/Evento.java
@@ -10,12 +10,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 
 @Data
@@ -36,6 +40,8 @@ public class Evento {
     private String descricao;
 
     @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime dataDoEvento;
 
     @Column(nullable = false)
@@ -43,4 +49,19 @@ public class Evento {
 
     @ManyToOne
     private Usuario organizador;
+
+    public Evento(
+        String nomeDoEvento,
+        String descricao,
+        LocalDateTime dataDoEvento,
+        String endereco,
+        Usuario organizador
+    ) {
+
+        this.nomeDoEvento = nomeDoEvento;
+        this.descricao = descricao;
+        this.dataDoEvento = dataDoEvento;
+        this.endereco = endereco;
+        this.organizador = organizador;
+    }
 }
